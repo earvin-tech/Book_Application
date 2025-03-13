4636bfaea6b3cbc3710a3157446a23926286c7b0
earvin-tech <wsl@Earvin>
2025-03-14 02:04:03 +1100

    feat: 404 route and error handler middleware

--- src/server.js
+++ src/server.js
@@ -13,6 +13,24 @@ app.get("/", (request, response) => {
     });
 });
 
+app.get("*", (request, response) => {
+    console.log("User tried to visit" + request.path);
+    response.status(404).json({
+        message: "Page not found.",
+        attemptedPath: request.path 
+    });
+});
+
+// Error handling catcher
+app.use((error, request, response, next) => {
+    console.log("Error occurred in the server.");
+    console.log(JSON.stringify(error));
+    response.json({
+        errors: request.body.errors,
+        message: error.message
+    });
+});
+
 app.get("/databaseHealth", (request, response) => {
     let databaseState = mongoose.connection.readyState;
     let databaseName = mongoose.connection.name;
