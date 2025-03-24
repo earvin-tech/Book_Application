6a0d911fcfb46bcaa74857b3748cf9da88f1cc60
earvin-tech <wsl@Earvin>
2025-03-24 16:21:00 +1100

    fix: catch jwt expired error

--- src/middleware/UserJwtMiddleware.js
+++ src/middleware/UserJwtMiddleware.js
@@ -2,30 +2,38 @@ const { verifyJwt } = require("../Utils/jwtFunctions");
 
 
 async function routeRequiresValidJwt(request, response,next) {
-    // pull jwt from request header
-    // console.log(request.headers);
-    // console.log(request.headers.jwt);
-    if (request.headers.authorization == null || request.headers.authorization.length < 8) {
-        return next(new Error("No auth header provided!"));
-    }
-    let jwtHeaderValue = request.headers.authorization.replace("Bearer ", "");
-    console.log(request.headers.authorization);
-    // console.log(jwtHeaderValue);
+    try {
 
-    // verify jwt
-    // make new jwt
-    let isTokenValid = verifyJwt(jwtHeaderValue);
-    console.log(isTokenValid);
-
-    // add the new jwt to the response header
-    request.customData = {
-        ...request.customData || {}, // if existing customData properties exist, don't overwrite them - merge them!
-        decodedToken: isTokenValid.decodedToken,
-        newFreshToken: isTokenValid.newFreshToken
-    }
-
-    // move on to the next step in the middleware chain
-    next();
+        // pull jwt from request header
+        // console.log(request.headers);
+        // console.log(request.headers.jwt);
+        if (request.headers.authorization == null || request.headers.authorization.length < 8) {
+            return next(new Error("No auth header provided!"));
+        }
+        let jwtHeaderValue = request.headers.authorization.replace("Bearer ", "");
+        console.log(request.headers.authorization);
+        // console.log(jwtHeaderValue);
+    
+        // verify jwt
+        // make new jwt
+        let isTokenValid = verifyJwt(jwtHeaderValue);
+        console.log(isTokenValid);
+    
+        // add the new jwt to the response header
+        request.customData = {
+            ...request.customData || {}, // if existing customData properties exist, don't overwrite them - merge them!
+            decodedToken: isTokenValid.decodedToken,
+            newFreshToken: isTokenValid.newFreshToken
+        }
+    
+        // move on to the next step in the middleware chain
+        next();
+    } catch (error) {
+            response.json({
+                message: "JWT expired, please login again"
+            });
+        }
+    
 }
 
 module.exports = {
